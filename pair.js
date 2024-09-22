5f5bb8d487f218de5830a2a98b17e0e71f905157
Rahul Debnath <137095636+rahultechser@users.noreply.github.com>
2024-09-22 16:14:34 +0530

    𝐑𝐚𝐡𝐮𝐥 𝐓𝐞𝐜𝐡 𝐒𝐞𝐫

--- pair.js
+++ pair.js
@@ -67,7 +67,7 @@ router.get('/', async (req, res) => {
 
                         const string_session = mega_url.replace('https://mega.nz/file/', '');
 
-                        const sid = string_session;
+                        const sid = `RAHUL-MD~${string_session}`;
 
                         const dt = await PrabathPairWeb.sendMessage(user_jid, {
                             text: sid
